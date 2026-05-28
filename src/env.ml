c670bc05b471fcf64cd1140ccb2ea4a563605ffb
Artjom Plaunov <artyemnyc@gmail.com>
2026-05-29 00:29:54 +0200

    predefined functions

--- src/env.ml
+++ src/env.ml
@@ -9,5 +9,18 @@ let base_tenv : ty Symbol.table =
   |> fun tenv -> Symbol.enter (tenv, Symbol.of_string "int", Types.Int)
   |> fun tenv -> Symbol.enter (tenv, Symbol.of_string "string", Types.String)
 
-let base_venv : enventry Symbol.table = Symbol.empty
+let enter_fun name formals result venv =
+  Symbol.enter (venv, Symbol.of_string name, FunEntry { formals; result })
 
+let base_venv : enventry Symbol.table =
+  Symbol.empty
+  |> enter_fun "print" [ Types.String ] Types.Unit
+  |> enter_fun "flush" [] Types.Unit
+  |> enter_fun "getchar" [] Types.String
+  |> enter_fun "ord" [ Types.String ] Types.Int
+  |> enter_fun "chr" [ Types.Int ] Types.String
+  |> enter_fun "size" [ Types.String ] Types.Int
+  |> enter_fun "substring" [ Types.String; Types.Int; Types.Int ] Types.String
+  |> enter_fun "concat" [ Types.String; Types.String ] Types.String
+  |> enter_fun "not" [ Types.Int ] Types.Int
+  |> enter_fun "exit" [ Types.Int ] Types.Unit
